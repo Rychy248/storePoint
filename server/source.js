1d773a819dd832f1b447b9c1ebdeed920ad94917
Rychy <jorgeajrhh@gmail.com>
2023-07-26 19:42:49 -0600

    first commit

---
+++ server/source.js
@@ -0,0 +1,9 @@
+
+
+
+// OFICIAL DOCUMENTATION
+// https://sequelize.org/
+
+
+// Fast Guide for Sequelize, Typescript/Nodejs ORM for MySQL
+// https://www.digitalocean.com/community/tutorials/how-to-use-sequelize-with-node-js-and-mysql#step-4-creating-associations-using-sequelize
